131a6711af82128dc03166cac60a7418c3764d8d
Nathan Hietala <Nrhietala@gmail.com>
2021-01-21 15:12:21 -0600

    MVP

--- src/App.js
+++ src/App.js
@@ -7,36 +7,38 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-// Context API
+// Import Context API's to set up Providers
 import { ProductContext } from '../src/contexts/ProductContext';
+import { CartContext } from '../src/contexts/CartContext';
 
-const ProductContext = useContext()
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		setCart({
+		setCart([
 			...cart,
 			item
-		});
+		]);
 	};
 	console.log(cart);
 
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-			<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
